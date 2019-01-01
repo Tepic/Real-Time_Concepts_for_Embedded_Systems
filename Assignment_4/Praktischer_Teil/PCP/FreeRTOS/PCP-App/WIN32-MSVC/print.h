204d080b48fe97810463ffb2b496f8d55e9bba5d
Ivan <ivanantunovic07@gmail.com>
2019-01-01 16:53:19 +0100

    added vPrintStringLn function in print.h

--- Assignment_4/Praktischer_Teil/PCP/FreeRTOS/PCP-App/WIN32-MSVC/print.h
+++ Assignment_4/Praktischer_Teil/PCP/FreeRTOS/PCP-App/WIN32-MSVC/print.h
@@ -9,14 +9,26 @@
 /* Kernel includes. */
 #include "FreeRTOS.h"
 
+static void vPrintStringLn(const char *pcString);
 static void vPrintString(const char *pcString);
 
-static void vPrintString(const char *pcString)
+void vPrintStringLn(const char *pcString)
 {
 	/* Write the string to stdout, using a critical section as a crude method of mutual exclusion. */
 	taskENTER_CRITICAL();
 	{
-		printf("%s\n", pcString);
+		printf("%s", pcString);
+		fflush(stdout);
+	}
+	taskEXIT_CRITICAL();
+}
+
+void vPrintString(const char *pcString)
+{
+	/* Write the string to stdout, using a critical section as a crude method of mutual exclusion. */
+	taskENTER_CRITICAL();
+	{
+		printf("%s", pcString);
 		fflush(stdout);
 	}
 	taskEXIT_CRITICAL();
